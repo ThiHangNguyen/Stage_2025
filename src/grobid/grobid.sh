95908643409c1ac42b51fec94ff95306ac378817
Thi Hang Nguyen <nguyth@octal33.iro.umontreal.ca>
2025-07-11 18:16:16 -0400

    traitement 100 nouveaux articles avec grobid

--- src/grobid/grobid.sh
+++ src/grobid/grobid.sh
@@ -2,12 +2,12 @@
 
 #!/bin/bash 
 
-INPUT_DIR="/Tmp/nguyth/project/Stage_2025/data/pdfs/pdf_2cols" 
-OUTPUT_DIR="/Tmp/nguyth/project/Stage_2025/data/raw/xml_grobid/xml_2cols" 
+INPUT_DIR="/Tmp/nguyth/project/Stage_2025/data/pdfs" 
+OUTPUT_DIR="/Tmp/nguyth/project/Stage_2025/data/raw/xml_grobid/" 
 mkdir -p "$OUTPUT_DIR"
 
 for f in "$INPUT_DIR"/*.pdf; do 
 	b=$(basename "$f" .pdf) 
-	echo "Processing $b..." 
+	echo "Processing $f..." 
 	curl -s -X POST http://localhost:8070/api/processFulltextDocument -F input=@"$f" -F consolidateHeader=1 -F consolidateCitations=1 -F consolidateFunding=1 -F includeRawAffiliations=1 -F includeRawCitations=1 -F includeRawCopyright=1 -F segmentSentences=1 -F generateIDs=1 -F teiCoordinates=persName -F teiCoordinates=figure -F teiCoordinates=ref -F teiCoordinates=biblStruct -F teiCoordinates=formula -F teiCoordinates=head -F teiCoordinates=label -F teiCoordinates=figureDesc -F teiCoordinates=table -F teiCoordinates=tr -F teiCoordinates=s -o "$OUTPUT_DIR/$b.xml" 
 done 
