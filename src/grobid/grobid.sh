8184bb9c19b07d8473717c2ef889295cfb0fe35b
Thi Hang Nguyen <nguyth@octal33.iro.umontreal.ca>
2025-07-24 14:04:12 -0400

    300 xml par grobid

--- src/grobid/grobid.sh
+++ src/grobid/grobid.sh
@@ -22,7 +22,7 @@ for journal in "${journals[@]}"; do
         [ -e "$f" ] || continue  # ignore si aucun fichier PDF
         b=$(basename "$f" .pdf)
         echo "  -> Traitement de $b.pdf..."
-        curl -s -X POST http://localhost:8070/api/processFulltextDocument -F input=@"$f" -F consolidateHeader=1 -F consolidateCitations=1 -F consolidateFunding=1 -F includeRawAffiliations=1 -F includeRawCitations=1 -F includeRawCopyright=1 -F segmentSentences=1 -F generateIDs=1 -F teiCoordinates=persName -F teiCoordinates=figure -F teiCoordinates=ref -F teiCoordinates=biblStruct -F teiCoordinates=formula -F teiCoordinates=head -F teiCoordinates=label -F teiCoordinates=figureDesc -F teiCoordinates=table -F teiCoordinates=tr -F teiCoordinates=s -o "$OUTPUT_DIR/$b.xml"
+        curl -s -X POST http://localhost:8070/api/processFulltextDocument -F input=@"$f" -F consolidateHeader=1 -F consolidateCitations=1 -F consolidateFunding=1 -F includeRawAffiliations=1 -F includeRawCitations=1 -F includeRawCopyright=1 -F segmentSentences=1 -F withSegment=1 -F generateIDs=1 -F teiCoordinates=persName -F teiCoordinates=figure -F teiCoordinates=ref -F teiCoordinates=biblStruct -F teiCoordinates=formula -F teiCoordinates=head -F teiCoordinates=label -F teiCoordinates=figureDesc -F teiCoordinates=table -F teiCoordinates=tr -F teiCoordinates=s -o "$OUTPUT_DIR/$b.xml"
 
     done
 done
