eaee87d153be1ac8a3c00677776466199d325189
Thi Hang Nguyen <nguyth@octal33.iro.umontreal.ca>
2025-09-04 16:28:53 -0400

    couter for grobid

--- src/grobid/grobid.sh
+++ src/grobid/grobid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Liste des identifiants de journaux
-journals=("haf18")
+journals=("test10")
 
 # Répertoire racine des PDF et des sorties XML
 PDF_ROOT="/Tmp/nguyth/project/Stage_2025/data/pdfs"
@@ -11,6 +11,8 @@ XML_ROOT="/Tmp/nguyth/project/Stage_2025/data/raw/xml_grobid"
 mkdir -p "$XML_ROOT"
 
 # Lancer le traitement pour chaque journal
+SECONDS=0
+
 for journal in "${journals[@]}"; do
     INPUT_DIR="$PDF_ROOT/$journal"
     OUTPUT_DIR="$XML_ROOT/$journal"
@@ -22,8 +24,10 @@ for journal in "${journals[@]}"; do
         [ -e "$f" ] || continue  # ignore si aucun fichier PDF
         b=$(basename "$f" .pdf)
         echo "  -> Traitement de $b.pdf..."
-        curl -s -X POST http://localhost:8070/api/processFulltextDocument -F input=@"$f" -F consolidateHeader=1 -F consolidateCitations=1 -F consolidateFunding=1 -F includeRawAffiliations=1 -F includeRawCitations=1 -F includeRawCopyright=1 -F segmentSentences=1 -F withSegment=1 -F generateIDs=1 -F teiCoordinates=persName -F teiCoordinates=figure -F teiCoordinates=ref -F teiCoordinates=biblStruct -F teiCoordinates=formula -F teiCoordinates=head -F teiCoordinates=label -F teiCoordinates=figureDesc -F teiCoordinates=table -F teiCoordinates=tr -F teiCoordinates=s -o "$OUTPUT_DIR/$b.xml"
+        curl -s -X POST http://localhost:8070/api/processFulltextDocument -F input=@"$f" -F consolidateHeader=1 -F consolidateCitations=1 -F consolidateFunding=1 -F includeRawAffiliations=1 -F includeRawCitations=1 -F includeRawCopyright=1 -F segmentSentences=1 -F withSegment=1 -F generateIDs=1 -F teiCoordinates=persName -F teiCoordinates=figure -F teiCoordinates=ref -F teiCoordinates=biblStruct -F teiCoordinates=formula -F teiCoordinates=head -F teiCoordinates=label -F teiCoordinates=figureDesc -F teiCoordinates=table -F teiCoordinates=tr -F teiCoordinates=s 
+        # -o "$OUTPUT_DIR/$b.xml"
 
     done
 done
+echo "Temps total : $SECONDS secondes" 
 
